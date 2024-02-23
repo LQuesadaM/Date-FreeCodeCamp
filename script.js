090b3ab1161dbadd7d9dbed5cd724494643745aa
LQuesadaM <luisquesada163@gmail.com>
2024-02-23 16:48:26 -0300

    const with Date contructor methods

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 
+const date = new Date(); // constructor
+
+const day = date.getDate();
+
+/* the first month of the year starts with zero). */
+const month = date.getMonth() + 1; 
+
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
